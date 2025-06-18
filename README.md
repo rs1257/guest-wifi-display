f74b06c1b40c777e5dce6972ef75acc715c08995
Ryan Smith <ryansmith300082@gmail.com>
2025-06-18 20:26:54 +0100

    feat: initial code to write to screen

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # guest-wifi-display
+
+# Create venv
+
+We need the site packages for it to work by running the command `python -m venv .venv --system-site-packages`
---
+++ main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import logging
+from driver.epd2in13_V4 import EPD
+from driver.epdconfig import module_exit
+from PIL import Image,ImageDraw,ImageFont
+
+logging.basicConfig(level=logging.DEBUG)
+
+try:    
+    epd = EPD()
+    logging.info("init and Clear")
+    epd.init()
+    epd.Clear(0xFF)
+
+    # Drawing on the image
+    font20 = ImageFont.truetype('Font.ttc', 20)
+    
+  
+    logging.info("1.Drawing on the image...")
+    image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
+    draw = ImageDraw.Draw(image)
+    draw.text((0, 0), "Scan to join:", font = font20, fill = 0)
+    draw.text((0, 20), "Ryan's Mesh Guest", font = font20, fill = 0)
+    bmp = Image.open('wifi.bmp')
+    image.paste(bmp, (int(epd.width/2 + 20), 50)) 
+    epd.display(epd.getbuffer(image))
+    epd.sleep()
+        
+except IOError as e:
+    logging.info(e)
+    
+except KeyboardInterrupt:    
+    logging.info("ctrl + c:")
+    module_exit(cleanup=True)
+    exit()
